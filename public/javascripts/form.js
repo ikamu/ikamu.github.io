9248668b1a44485d4a2b00c3fcb2233f80ee0a94
Félix Bélanger-Robillard <f.brobillard@gmail.com>
2016-10-26 18:37:43 -0400

    Carte pleine largeur

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,20 +1,9 @@
 // Auteur: Félix Bélanger-Robillard
 
 
-var unit = document.getElementBtId("unit").value;
 
-unit.value="trololol";
+var data = $('form').serializeArray();
 
-// var type = document.getElementBtId("type").value;
-
-// var branche = document.getElementBtId("branche").value;
-
-// var participants = document.getElementBtId("nbparticipants").value;
-
-// var autres = document.getElementBtId("autres").value;
-
-// var inscription = {unite: unit, type: type, branche: branche, participants: participants, autres: autres};
-
-console.log("YOLO");
-
-// console.log(inscription.unite);
+var helper = function() {
+  console.log(data.unit);
+}
